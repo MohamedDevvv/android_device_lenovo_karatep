0015e68e82b6c25dbb12c08d2f3ae435f50c01ea
karthick111 <karthickrko61@gmail.com>
2017-10-09 18:49:29 +0200

    karate: Import camera configs

---
+++ product/qcom-camera.mk
@@ -0,0 +1,7 @@
+# Camera
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/configs/camera/imx219_karatepof_chromatix.xml:system/etc/camera/imx219_karatepof_chromatix.xml \
+    $(DEVICE_PATH)/configs/camera/imx219_karatepqt_chromatix.xml:system/etc/camera/imx219_karatepqt_chromatix.xml \
+    $(DEVICE_PATH)/configs/camera/lenovo_karatep_camera.xml:system/etc/camera/lenovo_karatep_camera.xml \
+    $(DEVICE_PATH)/configs/camera/s5k3p3_karatepof_chromatix.xml:system/etc/camera/s5k3p3_karatepof_chromatix.xml \
+    $(DEVICE_PATH)/configs/camera/s5k3p3_karatepsy_chromatix.xml:system/etc/camera/s5k3p3_karatepsy_chromatix.xml
