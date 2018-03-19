d27d224f9675145a18a8be885ddd4ce270bd50f9
karthick111 <karthickrko61@gmail.com>
2018-03-19 20:37:56 +0100

    karatep: Copy camera configs to vendor

--- product/qcom-camera.mk
+++ product/qcom-camera.mk
@@ -1,7 +1,7 @@
 # Camera
 PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/configs/camera/imx219_karatepof_chromatix.xml:system/etc/camera/imx219_karatepof_chromatix.xml \
-    $(DEVICE_PATH)/configs/camera/imx219_karatepqt_chromatix.xml:system/etc/camera/imx219_karatepqt_chromatix.xml \
-    $(DEVICE_PATH)/configs/camera/lenovo_karatep_camera.xml:system/etc/camera/lenovo_karatep_camera.xml \
-    $(DEVICE_PATH)/configs/camera/s5k3p3_karatepof_chromatix.xml:system/etc/camera/s5k3p3_karatepof_chromatix.xml \
-    $(DEVICE_PATH)/configs/camera/s5k3p3_karatepsy_chromatix.xml:system/etc/camera/s5k3p3_karatepsy_chromatix.xml
+    $(DEVICE_PATH)/configs/camera/imx219_karatepof_chromatix.xml:$(TARGET_COPY_OUT_VENDOR)/etc/camera/imx219_karatepof_chromatix.xml \
+    $(DEVICE_PATH)/configs/camera/imx219_karatepqt_chromatix.xml:$(TARGET_COPY_OUT_VENDOR)/etc/camera/imx219_karatepqt_chromatix.xml \
+    $(DEVICE_PATH)/configs/camera/lenovo_karatep_camera.xml:$(TARGET_COPY_OUT_VENDOR)/etc/camera/lenovo_karatep_camera.xml \
+    $(DEVICE_PATH)/configs/camera/s5k3p3_karatepof_chromatix.xml:$(TARGET_COPY_OUT_VENDOR)/etc/camera/s5k3p3_karatepof_chromatix.xml \
+    $(DEVICE_PATH)/configs/camera/s5k3p3_karatepsy_chromatix.xml:$(TARGET_COPY_OUT_VENDOR)/etc/camera/s5k3p3_karatepsy_chromatix.xml
