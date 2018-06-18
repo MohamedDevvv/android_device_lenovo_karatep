1b15e4657153dd91b2f434f5236367214d64a5bb
karthick111 <karthickrko61@gmail.com>
2018-06-19 01:28:35 +0530

    karatep: Add device specific button jack keylayout

---
+++ product/keylayout.mk
@@ -0,0 +1,3 @@
+# Keylayouts
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/keylayout/msm8937-karatep-snd-card_Button_Jack.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/msm8937-karatep-snd-card_Button_Jack.kl
